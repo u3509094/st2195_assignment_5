848d1d54bc94479e7e16f40589eb6cd93493e98b
Lee Hin Sing Raymond <u3509094@connect.hku.hk>
2022-03-06 03:02:21 +0800

    Changed the argument of range function from 1000 to 1001

--- foo.py
+++ foo.py
@@ -12,7 +12,7 @@ Store all the integers that are multiples of 2 or 5 or 7 that are lower
 or equal to 1000 (excluding doubles)
 '''
 x = []
-for i in range(1000):
+for i in range(1001):
  if (is_divisible_by_k(x, 2) & is_divisible_by_k(x, 3)) |
 is_divisible_by_k(x, 7):
  x.append(i)
