2bd5b6e97742709298dee04eb6188bf3f051654a
Lee Hin Sing Raymond <u3509094@connect.hku.hk>
2022-03-06 03:01:32 +0800

    Changed the data type of x from immutable tuple to mutable list

--- foo.py
+++ foo.py
@@ -11,7 +11,7 @@ def is_divisible_by_k(x, k):
 Store all the integers that are multiples of 2 or 5 or 7 that are lower 
 or equal to 1000 (excluding doubles)
 '''
-x = ()
+x = []
 for i in range(1000):
  if (is_divisible_by_k(x, 2) & is_divisible_by_k(x, 3)) |
 is_divisible_by_k(x, 7):
