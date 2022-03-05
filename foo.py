6b6c8005e52cd1b4e7045dac53c5f763c2ccadb7
Lee Hin Sing Raymond <u3509094@connect.hku.hk>
2022-03-06 03:03:41 +0800

    Fixed the SyntaxError in the if function

--- foo.py
+++ foo.py
@@ -13,9 +13,8 @@ or equal to 1000 (excluding doubles)
 '''
 x = []
 for i in range(1001):
- if (is_divisible_by_k(x, 2) & is_divisible_by_k(x, 3)) |
-is_divisible_by_k(x, 7):
- x.append(i)
+ if (is_divisible_by_k(x, 2) & is_divisible_by_k(x, 3)) | is_divisible_by_k(x, 7):
+  x.append(i)
  
 '''
 Sum all the integers that are multiples of 2 or 5 or 7 that are lower 
